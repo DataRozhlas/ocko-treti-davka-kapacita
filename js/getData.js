f74ef092a8a33821c4f1553907594940af8eee7c
Petr Kočí <petr.koci@samizdat.cz>
2021-11-02 13:52:40 +0100

    volná a maximální kapacita po dnech

--- js/getData.js
+++ js/getData.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 // read JSON data in node.js
 
 const getData = (url) => {
@@ -16,8 +18,26 @@ const getData = (url) => {
 getData(
   "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-rezervace.min.json"
 ).then((data) => {
-  const mista = JSON.parse(data).data.filter(
-    (misto) => misto.kalendar_ockovani === "V3" && misto.maximalni_kapacita > 0
-  );
-  console.log(mista);
+  const mista = JSON.parse(data)
+    .data.filter(
+      (misto) =>
+        misto.kalendar_ockovani === "V3" && misto.maximalni_kapacita > 0 //místa s kapacitou na třetí dávku
+    )
+    .map((misto) => ({ ...misto, date: Date.parse(misto.datum) }))
+    .sort((a, b) => a.date - b.date);
+
+  const dny = mista.reduce((acc, curr) => {
+    if (acc[curr.date]) {
+      acc[curr.date].volna_kapacita += curr.volna_kapacita;
+      acc[curr.date].maximalni_kapacita += curr.maximalni_kapacita;
+    } else {
+      acc[curr.date] = {
+        volna_kapacita: curr.volna_kapacita,
+        maximalni_kapacita: curr.maximalni_kapacita,
+      };
+    }
+    return acc;
+  }, {});
+  // write data to file
+  fs.writeFileSync("./data/dny.json", JSON.stringify(dny, null, 2), "utf8");
 });
