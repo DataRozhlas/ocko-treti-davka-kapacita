89d16c265f0b19691b4e8b842f63c9e5364321aa
Petr Kočí <petr.koci@samizdat.cz>
2021-11-10 15:14:13 +0100

    relativně

--- js/ChartProockovanost.jsx
+++ js/ChartProockovanost.jsx
@@ -11,7 +11,7 @@ function ChartProockovanost({ data }) {
       text:
         data.name !== "Všechny věkové skupiny"
           ? `Věková skupina ${data.name} let`
-          : data.name,
+          : `${data.name} (včetně malých dětí)`,
     },
     credits: {
       enabled: false,
@@ -19,24 +19,34 @@ function ChartProockovanost({ data }) {
     xAxis: {
       type: "datetime",
     },
+    yAxis: {
+      max: 100,
+      title: {
+        text: "procento z dané věkové skupuiny",
+      },
+      labels: {
+        format: "{value} %",
+      },
+    },
+
     series: [
       {
         name: "1. dávka",
         data: Object.keys(data.davka1)
           .sort((a, b) => Number(a) - Number(b))
-          .map((k) => [Number(k), data.davka1[k]]),
+          .map((k) => [Number(k), (data.davka1[k] / (data.Z + data.M)) * 100]),
       },
       {
         name: "2. dávka",
         data: Object.keys(data.davka2)
           .sort((a, b) => Number(a) - Number(b))
-          .map((k) => [Number(k), data.davka2[k]]),
+          .map((k) => [Number(k), (data.davka2[k] / (data.Z + data.M)) * 100]),
       },
       {
         name: "3. dávka",
         data: Object.keys(data.davka3)
           .sort((a, b) => Number(a) - Number(b))
-          .map((k) => [Number(k), data.davka3[k]]),
+          .map((k) => [Number(k), (data.davka3[k] / (data.Z + data.M)) * 100]),
       },
     ],
   });
